2219b35df827d177178b6fa4ba7ad7c583511cd8
lucopa <lcobpar754@g.educaand.es>
2024-10-08 17:39:45 +0200

    Activity registro hecho

--- app/src/main/java/com/example/cinerec_app/Registro.java
+++ app/src/main/java/com/example/cinerec_app/Registro.java
@@ -1,12 +1,16 @@
 package com.example.cinerec_app;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class Registro extends AppCompatActivity {
 
@@ -15,6 +19,12 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_registro);
+
+       ImageView gifRegistro = findViewById(R.id.gifRegistro);
+
+        Glide.with(this)
+                .load(R.drawable.unirse)
+                .into(gifRegistro);
 
     }
 }
