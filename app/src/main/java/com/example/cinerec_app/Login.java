8d4de06e330873c9adcab255c47fb3296c61dce8
lucopa <lcobpar754@g.educaand.es>
2024-10-09 11:52:06 +0200

    Registro activity realizado y funcional

--- app/src/main/java/com/example/cinerec_app/Login.java
+++ app/src/main/java/com/example/cinerec_app/Login.java
@@ -1,6 +1,9 @@
 package com.example.cinerec_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    TextView text_Registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
 
+        text_Registro = findViewById(R.id.textoRegistro);
+
+        text_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Registro.class));
+            }
+        });
     }
 }
