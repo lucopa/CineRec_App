01c4cc85503186d923a4f5fe041b1010b448e355
lucopa <lcobpar754@g.educaand.es>
2024-10-10 09:36:18 +0200

    Cerrar Sesion implementado

--- app/src/main/java/com/example/cinerec_app/MenuPrincipal.java
+++ app/src/main/java/com/example/cinerec_app/MenuPrincipal.java
@@ -1,16 +1,28 @@
 package com.example.cinerec_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MenuPrincipal extends AppCompatActivity {
 
     ImageView btnBack;
     ImageView tvTitleToolbar;
     ImageView menuGallery;
+    Button CerrarSesion;
+    FirebaseAuth firebaseAuth;
+
+
+    FirebaseUser user;
 
 
     @Override
@@ -20,12 +32,18 @@ public class MenuPrincipal extends AppCompatActivity {
 
         initViews();
         initListener();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+
     }
 
     private void initViews() {
         btnBack = findViewById(R.id.btn_back);
         tvTitleToolbar = findViewById(R.id.tv_title_toolbar);
         menuGallery = findViewById(R.id.menu_gallery_toolbar);
+        CerrarSesion = findViewById(R.id.CerrarSesion);
     }
 
     private void initListener() {
@@ -33,6 +51,18 @@ public class MenuPrincipal extends AppCompatActivity {
 
         menuGallery.setOnClickListener(v -> Snackbar.make(v, "Opening Gallery...", Snackbar.LENGTH_SHORT).show());
 
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalirAplicacion();
+            }
+        });
 
+    }
+
+    private void SalirAplicacion() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste sesión", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/cinerec_app/PantallaCarga.java
+++ app/src/main/java/com/example/cinerec_app/PantallaCarga.java
@@ -8,14 +8,20 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PantallaCarga extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_carga);
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
        final int TIEMPO = 5000;
 
@@ -24,6 +30,7 @@ public class PantallaCarga extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(PantallaCarga.this, MainActivity.class ));
                 finish();
+                verificarUsuario();
             }
         }, TIEMPO);
 
@@ -33,5 +40,17 @@ public class PantallaCarga extends AppCompatActivity {
                 .load(R.drawable.cine)
                 .into(imageView);
 
+    }
+
+    private void verificarUsuario(){
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser(); //obtenemos el usuario actual (que se ha registrado previamente y ha iniciado sesion)
+
+        if (firebaseUser == null) { // si el usuario no ha iniciado sesion previamente, va a al main
+            startActivity(new Intent(PantallaCarga.this, MainActivity.class));
+            finish();
+        } else {
+            startActivity(new Intent(PantallaCarga.this, MenuPrincipal.class));
+            finish();
+        }
     }
 }
