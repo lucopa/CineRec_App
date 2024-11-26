beb19d2161a4f293fe7d87bc4618cc1b75ee8c0e
lucopa <lcobpar754@g.educaand.es>
2024-11-26 11:59:57 +0100

    Apartado de informacion realizado

--- app/src/main/java/com/example/cinerec_app/MenuPrincipal.java
+++ app/src/main/java/com/example/cinerec_app/MenuPrincipal.java
@@ -2,11 +2,7 @@ package com.example.cinerec_app;
 
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,26 +10,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuPrincipal extends AppCompatActivity  {
 
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        toolbar = findViewById(R.id.toolbar);
-
-        // Configura la Toolbar como ActionBar
-        setSupportActionBar(toolbar);
-
-        // Configura el ActionBarDrawerToggle
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
-
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
 
 
         setUpNavigation();
@@ -57,4 +40,3 @@ public class MenuPrincipal extends AppCompatActivity  {
 
 
 }
-
